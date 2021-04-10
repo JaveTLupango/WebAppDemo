4dee082c9e4d332206405ab174a4ea3285f066ad
Jave <pangga.jave@gmail.com>
2021-04-10 13:18:04 +0800

    update

--- WebAppDemo/Controllers/RegisterController.cs
+++ WebAppDemo/Controllers/RegisterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppDemo.Models;
 
 namespace WebAppDemo.Controllers
 {
@@ -11,6 +12,19 @@ namespace WebAppDemo.Controllers
         // GET: Register
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(user model)
+        {
+            using (WebAppDemoEntities db = new WebAppDemoEntities())
+            {
+                //var userret = db.users.Where(x => x.username == model.username && x.password == model.password).FirstOrDefault();
+                db.users.Add(model);
+                db.SaveChanges();
+               
+            }
             return View();
         }
     }
